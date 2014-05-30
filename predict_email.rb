b06fa46715fbd0cd32cf2d04792a9f7c5ae61ad3
vipyne <vipyne@gmail.com>
2014-05-30 15:07:05 -0400

    Create first_name_dot_last_name method

--- predict_email.rb
+++ predict_email.rb
@@ -1,3 +1,38 @@
 # DATASET ###################################
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'dataset'
+
+name = "vanessa pyne"
+domain = "bah"
+
+
+# POTENIAL PATTERNS #########################
+class PotentialPatterns
+  def first name
+    first_name = name.match(/^[^\s]*/).to_s
+  end
+
+  def last name
+    name.slice! name.match(/.*\s/).to_s
+    last_name = name
+  end
+
+  def first_name_dot_last_name name, domain
+    first(name) + "." + last(name) + "@" + domain
+  end
+
+  def first_name_dot_last_initial name, domain
+  end
+
+  def first_initial_dot_last_name name, domain
+  end
+
+  def first_initial_dot_last_initial name, domain
+  end
+end
+
+
+test = PotentialPatterns.new
+puts test.first name
+
+puts test.first_name_dot_last_name name, domain
