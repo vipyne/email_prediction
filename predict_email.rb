1ae4a7859512f9ab6258c92b1d55fcbb08c5abc1
vipyne <vipyne@gmail.com>
2014-05-30 15:19:38 -0400

    Create name initial methods
    updated last method because it was destructive to name string

--- predict_email.rb
+++ predict_email.rb
@@ -9,12 +9,21 @@ domain = "bah"
 # POTENIAL PATTERNS #########################
 class PotentialPatterns
   def first name
-    first_name = name.match(/^[^\s]*/).to_s
+    name.match(/^[^\s]*/).to_s
   end
 
   def last name
-    name.slice! name.match(/.*\s/).to_s
-    last_name = name
+    to_cut = name.dup
+    to_cut.slice! name.match(/.*\s/).to_s
+    last_name = to_cut
+  end
+
+  def first_initial name
+    first(name).slice(0)
+  end
+
+  def last_initial name
+    last(name).slice(0)
   end
 
   def first_name_dot_last_name name, domain
@@ -22,6 +31,7 @@ class PotentialPatterns
   end
 
   def first_name_dot_last_initial name, domain
+    first_initial(name) + "." + last(name) + "@" + domain
   end
 
   def first_initial_dot_last_name name, domain
@@ -33,6 +43,9 @@ end
 
 
 test = PotentialPatterns.new
-puts test.first name
+puts test.first_initial name
 
 puts test.first_name_dot_last_name name, domain
+puts test.first_name_dot_last_initial name, domain
+# puts test.first_initial_dot_last_name name, domain
+# puts test.first_initial_dot_last_initial name, domain
